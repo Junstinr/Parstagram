8d0d84756f52cc228a26ad86a6f668f3de2a112d
Junstin01 <notjun01@gmail.com>
2022-03-15 16:04:17 -0400

    part 2 of parstagram!

--- Parstagram/AppDelegate.swift
+++ Parstagram/AppDelegate.swift
@@ -39,6 +39,13 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
+        /*
+        Parse.initialize(with: ParseClientConfiguration(block: {(configuration: ParseMutableClientConfiguration in configuration.applicationId = "Parstagram" configuration.server = "https://whispering-stream-51370.herokuapp.com/parse"
+            <#code#>
+        }))
+         */
+        
+        
     }
 
 
